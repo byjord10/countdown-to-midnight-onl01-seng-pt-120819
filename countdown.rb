9a0cf82db07b71325a908922ba648530273c8810
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-15 23:29:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
 #write your code here
 def countdown(seconds_to_midnight)
-  while seconds_to_midnight < 0
+  while seconds_to_midnight < 0 do
     puts "#{seconds_to_midnight} SECOND(S)!"
     seconds_to_midnight -= 1
-    sleep 1
   end
    "HAPPY NEW YEAR!"
 end
